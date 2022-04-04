7cb324feb37528b1fbff9cb627f84e349094f4d8
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-04-04 15:16:31 +0100

    m

---
+++ server/schemas/user.js
@@ -0,0 +1,15 @@
+export default {
+    name:'user',
+    title:'User',
+    type:'document',
+    fields:[{
+        name:'userName',
+        title:'Username',
+        type:'string'
+    },
+    {
+        name:'image',
+        title:'Image',
+        type:'string'
+    }]
+}
